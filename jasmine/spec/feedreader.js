17d1a06ca00e1287acc777808679fa420223f1dd
NithyaPriya Thangavelu <NithyaPriya_Thangavelu@NithyaPriyas-MacBook-Pro.local>
2016-10-15 16:32:51 -0400

    Added the Readme and final changes

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,47 +26,98 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('has a URL property defined and it is not empty', function() {
+            allFeeds.forEach(function(feeds) {
+                //checks if URL is defined
+                expect(feeds.url).toBeDefined();
+                //checks if URL is not empty 
+                expect(feeds.url.length).not.toBe(0);
+            });
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has a name property defined and it is not empty', function(){
+            allFeeds.forEach(function(feeds) {
+                //checks if name is defined
+                expect(feeds.name).toBeDefined();
+                //checks if name is not empty 
+                expect(feeds.name.length).not.toBe(0);
+            });
+         });
     });
 
-
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('element is hidden by default', function() {
+           expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* TODO: Write a test that ensures the menu changes
+        * visibility when the menu icon is clicked. This test
+        * should have two expectations: does the menu display when
+        * clicked and does it hide when clicked again.
+        */
+        it('display when clicked and does it hides when clicked again', function(){
+            var $menuIcon = $('.menu-icon-link');
+            //menu display when clicked at first so the menu-hidden tobe(false)
+            $menuIcon.trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            //menu display when clicked at again so the menu-hidden tobe(true)
+            $menuIcon.trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(true);                  
+        });
+
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries',function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done){
+            loadFeed(0, function(){
+                done();
+            });
+        });
+        it('has at least one entry in feed container', function(done){
+            expect($('.feed .entry').length).not.toBeLessThan(1);
+            done();
+        });
+    });    
 
     /* TODO: Write a new test suite named "New Feed Selection"
-
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    describe('New Feed Selection', function(){
+        var feedUrl;
+        beforeEach(function(done){
+            loadFeed(1,function(){
+                feedUrl = $("a.entry-link").attr("href");
+                loadFeed(2, done);
+            });
+        });
+
+        it('ensures when a new feed is loaded by the loadFeed function that the content actually changes', function(done){
+            var currentFeedUrl = $("a.entry-link").attr("href");
+            expect(currentFeedUrl).not.toBe(feedUrl);
+            done();
+        });
+    });
 }());
